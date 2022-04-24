fc614f7e52dbe6df2599e5de14320f920ff9eaf2
Jordan S <jpstanway@gmail.com>
2022-04-23 19:52:01 -0700

    return verbose option when prompted

--- port_scanner.py
+++ port_scanner.py
@@ -1,4 +1,5 @@
 import socket
+from common_ports import ports_and_services
 
 
 def get_ip(target):
@@ -7,32 +8,50 @@ def get_ip(target):
         ip = socket.gethostbyname(target)
     except:
         ip = target
-
     return ip
 
 
-def get_open_ports(target, port_range, verbose=False):
+def get_hostname(target):
+    host = ''
+    try:
+        host = socket.gethostbyaddr(target)[0]
+    except:
+        host = target
+    return host
 
+
+def get_open_ports(target, port_range, verbose=False):
     open_ports = []
     ip = get_ip(target)
+    host = get_hostname(target)
     ports = list(range(port_range[0], port_range[1]))
-    v_str = '''Open ports for {target} ({ip})\nPORT     SERVICE\n'''.format(
-        target=target, ip=ip)
+    v_str = '''Open ports for {host} ({ip})\nPORT     SERVICE\n'''.format(
+        host=host, ip=ip)
 
-    for p in ports:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
-        result = s.connect_ex((ip, int(p)))
+    try:
+        for p in ports:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(1)
+            result = s.connect_ex((ip, int(p)))
 
-        if result == 0:
-            open_ports.append(p)
+            if result == 0:
+                open_ports.append(p)
 
-        if verbose:
-            v_str += str(p) + '       ' + 'service\n'
+            if verbose:
+                v_str += str(p)
+                if p in ports_and_services:
+                    v_str += '      '
+                    v_str += ports_and_services[p]
+                v_str += '\n'
 
-        s.close()
+            s.close()
+    except:
+        if 'www.' in target:
+            return 'Error: Invalid hostname'
+        else:
+            return 'Error: Invalid IP address'
 
     if verbose:
-        print(v_str)
+        return v_str
 
     return(open_ports)

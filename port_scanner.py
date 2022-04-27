437d60479c57eb0c1f7cf8825dfac9ed4d854d9b
Jordan S <jpstanway@gmail.com>
2022-04-26 23:11:17 -0700

    handle errors and refactor verbose string

--- port_scanner.py
+++ port_scanner.py
@@ -33,36 +33,37 @@ def get_verbose_output(host, ip):
 def get_verbose_spacing(port):
     total = 9
     space = ' '
-    return (total - len(port)) * space
+    return (total - len(str(port))) * space
 
 
 def get_open_ports(target, port_range, verbose=False):
     open_ports = []
     ip = get_ip(target)
     host = get_hostname(target)
-    ports = list(range(port_range[0], port_range[1]))
+    ports = list(range(port_range[0], port_range[1] + 1))
     v_str = get_verbose_output(host, ip)
 
-    for idx, p in enumerate(ports):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
-        result = s.connect_ex((ip, int(p)))
+    try:
+        for p in ports:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(1)
+            result = s.connect_ex((ip, int(p)))
 
-        if result == 0:
-            open_ports.append(p)
+            if result == 0:
+                open_ports.append(p)
+
+                if verbose and p in ports_and_services:
+                    v_str += '\n'
+                    v_str += str(p)
+                    v_str += get_verbose_spacing(p)
+                    v_str += ports_and_services[p]
+
+            s.close()
+    except socket.gaierror:
+        if target[0].isnumeric():
+            return 'Error: Invalid IP address'
         else:
-            if target[0].isnumeric():
-                return 'Error: Invalid IP address'
-            else:
-                return 'Error: Invalid hostname'
-
-        if verbose and p in ports_and_services:
-            v_str += '\n'
-            v_str += str(p)
-            v_str += get_verbose_spacing(p)
-            v_str += ports_and_services[p]
-
-        s.close()
+            return 'Error: Invalid hostname'
 
     if verbose:
         return v_str
